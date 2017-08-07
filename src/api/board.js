6fcaed58bd80a7f57f1d3c62544b7fbb62395863
JohnDMathis <john.mathis@leankit.com>
2017-08-07 09:31:16 -0500

    add 'board.changes' and 'board.cards' methods

--- src/api/board.js
+++ src/api/board.js
@@ -15,6 +15,34 @@ module.exports = ( api, request ) => {
 		} );
 	};
 
+	api.board.changes = ( boardId, version ) => {
+		return request( {
+			url: `/io/board/${ boardId }/changes?version=${ version }`,
+			method: "get"
+		} );
+	};
+
+	api.board.cards = ( { boardId, cards, lanes} ) => {
+		let qs = "";
+		if( cards ){
+			if( typeof( cards ) === "object" ) {
+				cards = cards.join();
+			}
+			qs+= `cards=${ cards }&`;
+		}
+		if( lanes ) {
+			if( typeof( lanes ) === "object" ) {
+				lanes = lanes.join();
+			}
+			qs+= `lanes=${ lanes }`;
+		}
+		return request( {
+			url: `/io/board/${ boardId }/card?${ qs }`,
+			method: "get"
+		} );
+	};
+
+
 	api.board.customFields.list = boardId => {
 		return request( {
 			url: `/io/board/${ boardId }/customfield`,
